0342244b57d8292c6db37b018a7814c381ffaf8b
selmanmestanli <selmanmestanli@gmail.com>
2022-02-26 22:14:31 +0100

    Implement user and review model into the start.js

--- start.js
+++ start.js
@@ -23,6 +23,8 @@ mongoose.connection.on("error", (err) => {
 
 // import all of models
 require("./models/Store");
+require("./models/User");
+require("./models/Review");
 
 // Start our app!
 const app = require("./app");
