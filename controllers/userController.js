f10b6f05a07e03b86424a3de79ff1e88c92a3eb8
selmanmestanli <selmanmestanli@gmail.com>
2022-02-26 22:11:15 +0100

    Update user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const promisify = require("es6-promisify");
 
 exports.loginForm = (req, res) => {
 	res.render("login", { title: "Login" });
@@ -42,5 +44,30 @@ exports.validateRegister = (req, res, next) => {
 };
 
 exports.register = async (req, res, next) => {
+	const user = new User({
+		email: req.body.email,
+		name: req.body.name,
+	});
+	const register = promisify(User.register, User);
+	await register(user, req.body.password);
+	next();
+};
 
-}
+exports.account = (req, res) => {
+	res.render("account", { title: "Edit Your Account" });
+};
+
+exports.updateAccount = async (req, res) => {
+	const updates = {
+		name: req.body.name,
+		email: req.body.email,
+	};
+
+	const user = await User.findOneAndUpdate(
+		{ _id: req.user._id },
+		{ $set: updates },
+		{ new: true, runValidators: true, context: "query" }
+	);
+	req.flash("success", "Successfully updated the profile!");
+	res.redirect("back");
+};
